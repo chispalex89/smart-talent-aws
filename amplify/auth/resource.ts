a21610aeb1b655ac5f0130740eaf882cbb237488
Pablo Godoy <pablogodoy@bairesdev.com>
2025-03-30 19:55:24 -0600

    email smart-talent

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -11,13 +11,17 @@ export const auth = defineAuth({
       verificationEmailSubject:
         'Código de verificación para tu cuenta de Smart Talent',
       verificationEmailBody: (createCode) =>
-        `Utiliza este código para activar tu cuenta: ${createCode()}`,
+        `
+      <img src="https://main.d1pmhoqbi0rauz.amplifyapp.com/img/logo/logo-light-full.svg" alt="Smart Talent" width="200" />
+      <h3>¡Hola, bienvenido a Smart Talent!</h3>
+      <p>Utiliza este código para activar tu cuenta:</p><h1>${createCode()}</h1>
+      `,
+
       userInvitation: {
         emailSubject: '¡Bienvenido(a) a Smart Talent!',
         emailBody: (user, code) =>
-          `¡Estamos encantados que estés con nosotros! Puedes iniciar sesión con ${user()} y este código ${code()}`,
+          `<p>¡Estamos encantados que estés con nosotros!</p><p>Puedes iniciar sesión con <strong>${user()}</strong> y este código <strong>${code()}</strong></p>`,
       },
-      
     },
   },
 });
