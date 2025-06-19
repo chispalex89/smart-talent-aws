e871f9c8007063a6f9ce83f9e6a6715a503bb5ee
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-06-19 16:16:52 -0600

    added document type configuration and updated prisma schema

--- src/views/backOffice/components/document-types/list.tsx
+++ src/views/backOffice/components/document-types/list.tsx
@@ -6,12 +6,87 @@ import DataTable, {
 } from '@/components/shared/DataTable';
 import { useDocumentTypeList } from '../../hooks';
 import { renderIsDeletedToggle } from '../../../../helpers/renderActiveToggle';
-import { Card } from '@/components/ui';
+import { Card, Drawer, toast } from '@/components/ui';
 import { DocumentType } from '@prisma/client';
+import GenericForm from '../generic-form';
+import ActionTableColumn from '../generic-form/action-table-column';
+import apiService from '../../../../services/apiService';
+import Notification from '@/components/ui/Notification';
+import { D } from 'framer-motion/dist/types.d-O7VGXDJe';
 
-const DocumentTypeList = () => {
+const DocumentStatusList = () => {
   const { list, total, tableData, isLoading, setTableData, mutate } =
     useDocumentTypeList();
+
+  const [selectedRow, setSelectedRow] = React.useState<DocumentType>(
+    {} as DocumentType
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const handleEdit = (row: DocumentType) => {
+    setSelectedRow(row);
+    setIsDrawerOpen(true);
+  };
+
+  const handleStatusChange = async (
+    row: DocumentType,
+    isDeleted: boolean
+  ) => {};
+
+  const handleDelete = async (row: DocumentType) => {
+    try {
+      await apiService.delete(`/document-type/${row.id}`);
+      mutate();
+      toast.push(
+        <Notification type="info">
+          Documento de identificación eliminado correctamente
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } catch (error) {
+      console.error('Error deleting document type status:', error);
+      toast.push(
+        <Notification type="danger">
+          Error al eliminar el documento de identificación
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } finally {
+      setSelectedRow({} as DocumentType);
+      setIsDrawerOpen(false);
+    }
+  };
+
+    const handleApply = async (updatedRow: DocumentType) => {
+      try {
+        await apiService.put(`/document-type/${selectedRow.id}`, updatedRow);
+        mutate();
+        toast.push(
+          <Notification type="info">
+            Document type actualizado correctamente
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } catch (error) {
+        console.error('Error updating document type status:', error);
+        toast.push(
+          <Notification type="danger">
+            Error al actualizar el estado del documento de identificación
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } finally {
+        setIsDrawerOpen(false);
+        setSelectedRow({} as DocumentType);
+      }
+    };
 
   const handlePaginationChange = (page: number) => {
     const newTableData = cloneDeep(tableData);
@@ -38,7 +113,7 @@ const DocumentTypeList = () => {
     () => [
       {
         accessorKey: 'name',
-        header: 'Documentos de Identificación',
+        header: 'Documento de identificación',
         cell: (info) => info.getValue(),
       },
       {
@@ -72,28 +147,56 @@ const DocumentTypeList = () => {
             day: '2-digit',
           }),
       },
+      {
+        header: '',
+        id: 'action',
+        cell: (props) => (
+          <ActionTableColumn
+            row={props.row.original}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ),
+      },
     ],
     []
   );
 
   return (
-    <Card>
-      <DataTable
-        columns={columns}
-        data={list}
-        noData={!isLoading && list.length === 0}
-        loading={isLoading}
-        pagingData={{
-          total: total,
-          pageIndex: tableData.pageIndex as number,
-          pageSize: tableData.pageSize as number,
-        }}
-        onPaginationChange={handlePaginationChange}
-        onSelectChange={handleSelectChange}
-        onSort={handleSort}
-      />
-    </Card>
+    <>
+      <Card>
+        <DataTable
+          columns={columns}
+          data={list}
+          noData={!isLoading && list.length === 0}
+          loading={isLoading}
+          pagingData={{
+            total: total,
+            pageIndex: tableData.pageIndex as number,
+            pageSize: tableData.pageSize as number,
+          }}
+          onPaginationChange={handlePaginationChange}
+          onSelectChange={handleSelectChange}
+          onSort={handleSort}
+        />
+      </Card>
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        title="Detalles del Documento de Identificación"
+        closable={true}
+      >
+        <GenericForm
+          initialValues={selectedRow as DocumentType}
+          onSubmit={(item) => handleApply(item)}
+          onCancel={() => setIsDrawerOpen(false)}
+          submitButtonText="Guardar Cambios"
+          cancelButtonText="Cancelar"
+          subTitle="Complete los detalles del documento de identificación"
+        ></GenericForm>
+      </Drawer>
+    </>
   );
 };
 
-export default DocumentTypeList;
+export default DocumentStatusList;
