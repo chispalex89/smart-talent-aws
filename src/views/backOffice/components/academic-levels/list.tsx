f64e669269056865426e2ae6b1cc7a12356b534b
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-06-20 16:19:30 -0600

    academic levels done

--- src/views/backOffice/components/academic-levels/list.tsx
+++ src/views/backOffice/components/academic-levels/list.tsx
@@ -4,14 +4,89 @@ import DataTable, {
   ColumnDef,
   OnSortParam,
 } from '@/components/shared/DataTable';
-import { useAcademicLevelList } from '../../hooks';
+import  { useAcademicLevelList } from '../../hooks';
 import { renderIsDeletedToggle } from '../../../../helpers/renderActiveToggle';
-import { Card } from '@/components/ui';
+import { Card, Drawer, toast } from '@/components/ui';
 import { AcademicLevel } from '@prisma/client';
+import GenericForm from '../generic-form';
+import ActionTableColumn from '../generic-form/action-table-column';
+import apiService from '../../../../services/apiService';
+import Notification from '@/components/ui/Notification';
+import { D } from 'framer-motion/dist/types.d-O7VGXDJe';
 
 const AcademicLevelList = () => {
   const { list, total, tableData, isLoading, setTableData, mutate } =
     useAcademicLevelList();
+
+  const [selectedRow, setSelectedRow] = React.useState<AcademicLevel>(
+    {} as AcademicLevel
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const handleEdit = (row: AcademicLevel) => {
+  setSelectedRow(row);
+    setIsDrawerOpen(true);
+  };
+
+  const handleStatusChange = async (
+    row: AcademicLevel,
+    isDeleted: boolean
+  ) => {};
+
+  const handleDelete = async (row: AcademicLevel) => {
+    try {
+      await apiService.delete(`/academic-level/${row.id}`);
+      mutate();
+      toast.push(
+        <Notification type="info">
+          Nivel académico eliminada correctamente
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } catch (error) {
+      console.error('Error deleting a driver license :', error);
+      toast.push(
+        <Notification type="danger">
+          Error al eliminar el nivel académico
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } finally {
+      setSelectedRow({} as AcademicLevel);
+      setIsDrawerOpen(false);
+    }
+  };
+
+    const handleApply = async (updatedRow: AcademicLevel) => {
+      try {
+        await apiService.put(`/academic-level/${selectedRow.id}`, updatedRow);
+        mutate();
+        toast.push(
+          <Notification type="info">
+            Nivel académico actualizado correctamente
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } catch (error) {
+        console.error('Error updating State:', error);
+        toast.push(
+          <Notification type="danger">
+            Error al actualizar el nivel académico
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } finally {
+        setIsDrawerOpen(false);
+        setSelectedRow({} as AcademicLevel);
+      }
+    };
 
   const handlePaginationChange = (page: number) => {
     const newTableData = cloneDeep(tableData);
@@ -38,7 +113,7 @@ const AcademicLevelList = () => {
     () => [
       {
         accessorKey: 'name',
-        header: 'Nivel Académico',
+        header: 'Departamento',
         cell: (info) => info.getValue(),
       },
       {
@@ -72,27 +147,55 @@ const AcademicLevelList = () => {
             day: '2-digit',
           }),
       },
+      {
+        header: '',
+        id: 'action',
+        cell: (props) => (
+          <ActionTableColumn
+            row={props.row.original}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ),
+      },
     ],
     []
   );
 
   return (
-    <Card>
-      <DataTable
-        columns={columns}
-        data={list}
-        noData={!isLoading && list.length === 0}
-        loading={isLoading}
-        pagingData={{
-          total: total,
-          pageIndex: tableData.pageIndex as number,
-          pageSize: tableData.pageSize as number,
-        }}
-        onPaginationChange={handlePaginationChange}
-        onSelectChange={handleSelectChange}
-        onSort={handleSort}
-      />
-    </Card>
+    <>
+      <Card>
+        <DataTable
+          columns={columns}
+          data={list}
+          noData={!isLoading && list.length === 0}
+          loading={isLoading}
+          pagingData={{
+            total: total,
+            pageIndex: tableData.pageIndex as number,
+            pageSize: tableData.pageSize as number,
+          }}
+          onPaginationChange={handlePaginationChange}
+          onSelectChange={handleSelectChange}
+          onSort={handleSort}
+        />
+      </Card>
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        title="Detalles del nivel académico"
+        closable={true}
+      >
+        <GenericForm
+          initialValues={selectedRow as AcademicLevel}
+          onSubmit={(item) => handleApply(item)}
+          onCancel={() => setIsDrawerOpen(false)}
+          submitButtonText="Guardar Cambios"
+          cancelButtonText="Cancelar"
+          subTitle="Complete los detalles del nivel académico"
+        ></GenericForm>
+      </Drawer>
+    </>
   );
 };
 
