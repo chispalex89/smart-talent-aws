00349c33d970fc67a2821ba168d6dcfbcb95612a
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-06-20 15:39:15 -0600

    cities done

--- src/views/backOffice/components/cities/list.tsx
+++ src/views/backOffice/components/cities/list.tsx
@@ -4,14 +4,89 @@ import DataTable, {
   ColumnDef,
   OnSortParam,
 } from '@/components/shared/DataTable';
-import { useCityList } from '../../hooks';
+import  { useCityList } from '../../hooks';
 import { renderIsDeletedToggle } from '../../../../helpers/renderActiveToggle';
-import { Card } from '@/components/ui';
+import { Card, Drawer, toast } from '@/components/ui';
 import { City } from '@prisma/client';
+import GenericForm from '../generic-form';
+import ActionTableColumn from '../generic-form/action-table-column';
+import apiService from '../../../../services/apiService';
+import Notification from '@/components/ui/Notification';
+import { D } from 'framer-motion/dist/types.d-O7VGXDJe';
 
-const CityList = () => {
+const CityStatusList = () => {
   const { list, total, tableData, isLoading, setTableData, mutate } =
     useCityList();
+
+  const [selectedRow, setSelectedRow] = React.useState<City>(
+    {} as City
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const handleEdit = (row: City) => {
+  setSelectedRow(row);
+    setIsDrawerOpen(true);
+  };
+
+  const handleStatusChange = async (
+    row: City,
+    isDeleted: boolean
+  ) => {};
+
+  const handleDelete = async (row: City) => {
+    try {
+      await apiService.delete(`/city/${row.id}`);
+      mutate();
+      toast.push(
+        <Notification type="info">
+          Ciudad eliminada correctamente
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } catch (error) {
+      console.error('Error deleting city:', error);
+      toast.push(
+        <Notification type="danger">
+          Error al eliminar la ciudad
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } finally {
+      setSelectedRow({} as City);
+      setIsDrawerOpen(false);
+    }
+  };
+
+    const handleApply = async (updatedRow: City) => {
+      try {
+        await apiService.put(`/city/${selectedRow.id}`, updatedRow);
+        mutate();
+        toast.push(
+          <Notification type="info">
+            Ciudad actualizado correctamente
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } catch (error) {
+        console.error('Error updating State:', error);
+        toast.push(
+          <Notification type="danger">
+            Error al actualizar la ciudad
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } finally {
+        setIsDrawerOpen(false);
+        setSelectedRow({} as City);
+      }
+    };
 
   const handlePaginationChange = (page: number) => {
     const newTableData = cloneDeep(tableData);
@@ -38,18 +113,40 @@ const CityList = () => {
     () => [
       {
         accessorKey: 'name',
-        header: 'Municipio',
+        header: 'Ciudad',
         cell: (info) => info.getValue(),
       },
       {
-        accessorKey: 'state.name',
+        accessorKey: 'description',
+        header: 'Descripción',
+        cell: (info) => info.getValue(),
+      },
+      {
+        accessorKey: 'state',
         header: 'Departamento',
-        cell: (info) => info.getValue(),
+        cell: (info) => {
+          const state = info.getValue();
+          return state && typeof state === 'object' && 'name' in state
+            ? state.name
+            : 'N/A';
+        },
       },
       {
-        accessorKey: 'state.country.name',
+        accessorKey: 'state',
         header: 'País',
-        cell: (info) => info.getValue(),
+        cell: (info) => {
+          const state = info.getValue();
+          const country =
+            state &&
+            typeof state === 'object' &&
+            'country' in state &&
+            state.country &&
+            typeof state.country === 'object' &&
+            'name' in state.country
+              ? state.country.name
+              : 'N/A';
+          return country;
+        },
       },
       {
         accessorKey: 'isDeleted',
@@ -77,28 +174,56 @@ const CityList = () => {
             day: '2-digit',
           }),
       },
+      {
+        header: '',
+        id: 'action',
+        cell: (props) => (
+          <ActionTableColumn
+            row={props.row.original}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ),
+      },
     ],
     []
   );
 
   return (
-    <Card>
-      <DataTable
-        columns={columns}
-        data={list}
-        noData={!isLoading && list.length === 0}
-        loading={isLoading}
-        pagingData={{
-          total: total,
-          pageIndex: tableData.pageIndex as number,
-          pageSize: tableData.pageSize as number,
-        }}
-        onPaginationChange={handlePaginationChange}
-        onSelectChange={handleSelectChange}
-        onSort={handleSort}
-      />
-    </Card>
+    <>
+      <Card>
+        <DataTable
+          columns={columns}
+          data={list}
+          noData={!isLoading && list.length === 0}
+          loading={isLoading}
+          pagingData={{
+            total: total,
+            pageIndex: tableData.pageIndex as number,
+            pageSize: tableData.pageSize as number,
+          }}
+          onPaginationChange={handlePaginationChange}
+          onSelectChange={handleSelectChange}
+          onSort={handleSort}
+        />
+      </Card>
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        title="Detalles del municipio"
+        closable={true}
+      >
+        <GenericForm
+          initialValues={selectedRow as City}
+          onSubmit={(item) => handleApply(item)}
+          onCancel={() => setIsDrawerOpen(false)}
+          submitButtonText="Guardar Cambios"
+          cancelButtonText="Cancelar"
+          subTitle="Complete los detalles del municipio"
+        ></GenericForm>
+      </Drawer>
+    </>
   );
 };
 
-export default CityList;
+export default CityStatusList;
