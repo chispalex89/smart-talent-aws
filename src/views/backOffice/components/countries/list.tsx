b63981481aca6253fd26dc60c0f011ac86e83317
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-06-20 13:15:17 -0600

    country done

--- src/views/backOffice/components/countries/list.tsx
+++ src/views/backOffice/components/countries/list.tsx
@@ -4,14 +4,89 @@ import DataTable, {
   ColumnDef,
   OnSortParam,
 } from '@/components/shared/DataTable';
-import { useCountryList } from '../../hooks';
+import  {useCountryList } from '../../hooks';
 import { renderIsDeletedToggle } from '../../../../helpers/renderActiveToggle';
-import { Card } from '@/components/ui';
+import { Card, Drawer, toast } from '@/components/ui';
 import { Country } from '@prisma/client';
+import GenericForm from '../generic-form';
+import ActionTableColumn from '../generic-form/action-table-column';
+import apiService from '../../../../services/apiService';
+import Notification from '@/components/ui/Notification';
+import { D } from 'framer-motion/dist/types.d-O7VGXDJe';
 
-const CountryList = () => {
+const CountryStatusList = () => {
   const { list, total, tableData, isLoading, setTableData, mutate } =
     useCountryList();
+
+  const [selectedRow, setSelectedRow] = React.useState<Country>(
+    {} as Country
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const handleEdit = (row: Country) => {
+  setSelectedRow(row);
+    setIsDrawerOpen(true);
+  };
+
+  const handleStatusChange = async (
+    row: Country,
+    isDeleted: boolean
+  ) => {};
+
+  const handleDelete = async (row: Country) => {
+    try {
+      await apiService.delete(`/country/${row.id}`);
+      mutate();
+      toast.push(
+        <Notification type="info">
+          País eliminado correctamente
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } catch (error) {
+      console.error('Error deleting country:', error);
+      toast.push(
+        <Notification type="danger">
+          Error al eliminar el país
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } finally {
+      setSelectedRow({} as Country);
+      setIsDrawerOpen(false);
+    }
+  };
+
+    const handleApply = async (updatedRow: Country) => {
+      try {
+        await apiService.put(`/country/${selectedRow.id}`, updatedRow);
+        mutate();
+        toast.push(
+          <Notification type="info">
+            País actualizado correctamente
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } catch (error) {
+        console.error('Error updating country:', error);
+        toast.push(
+          <Notification type="danger">
+            Error al actualizar el país
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } finally {
+        setIsDrawerOpen(false);
+        setSelectedRow({} as Country);
+      }
+    };
 
   const handlePaginationChange = (page: number) => {
     const newTableData = cloneDeep(tableData);
@@ -72,28 +147,56 @@ const CountryList = () => {
             day: '2-digit',
           }),
       },
+      {
+        header: '',
+        id: 'action',
+        cell: (props) => (
+          <ActionTableColumn
+            row={props.row.original}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ),
+      },
     ],
     []
   );
 
   return (
-    <Card>
-      <DataTable
-        columns={columns}
-        data={list}
-        noData={!isLoading && list.length === 0}
-        loading={isLoading}
-        pagingData={{
-          total: total,
-          pageIndex: tableData.pageIndex as number,
-          pageSize: tableData.pageSize as number,
-        }}
-        onPaginationChange={handlePaginationChange}
-        onSelectChange={handleSelectChange}
-        onSort={handleSort}
-      />
-    </Card>
+    <>
+      <Card>
+        <DataTable
+          columns={columns}
+          data={list}
+          noData={!isLoading && list.length === 0}
+          loading={isLoading}
+          pagingData={{
+            total: total,
+            pageIndex: tableData.pageIndex as number,
+            pageSize: tableData.pageSize as number,
+          }}
+          onPaginationChange={handlePaginationChange}
+          onSelectChange={handleSelectChange}
+          onSort={handleSort}
+        />
+      </Card>
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        title="Detalles del País"
+        closable={true}
+      >
+        <GenericForm
+          initialValues={selectedRow as Country}
+          onSubmit={(item) => handleApply(item)}
+          onCancel={() => setIsDrawerOpen(false)}
+          submitButtonText="Guardar Cambios"
+          cancelButtonText="Cancelar"
+          subTitle="Complete los detalles del país"
+        ></GenericForm>
+      </Drawer>
+    </>
   );
 };
 
-export default CountryList;
+export default CountryStatusList;
