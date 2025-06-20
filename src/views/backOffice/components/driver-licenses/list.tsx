be48b7c4a0f294654fdc311cddd49155e2aea5a4
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-06-20 16:07:16 -0600

    driver license done

--- src/views/backOffice/components/driver-licenses/list.tsx
+++ src/views/backOffice/components/driver-licenses/list.tsx
@@ -4,14 +4,89 @@ import DataTable, {
   ColumnDef,
   OnSortParam,
 } from '@/components/shared/DataTable';
-import { useDriverLicenseTypeList } from '../../hooks';
+import  { useDriverLicenseTypeList } from '../../hooks';
 import { renderIsDeletedToggle } from '../../../../helpers/renderActiveToggle';
-import { Card } from '@/components/ui';
+import { Card, Drawer, toast } from '@/components/ui';
 import { DriverLicense } from '@prisma/client';
+import GenericForm from '../generic-form';
+import ActionTableColumn from '../generic-form/action-table-column';
+import apiService from '../../../../services/apiService';
+import Notification from '@/components/ui/Notification';
+import { D } from 'framer-motion/dist/types.d-O7VGXDJe';
 
-const DriverLicenseTypeList = () => {
+const DriverLicenseStatusList = () => {
   const { list, total, tableData, isLoading, setTableData, mutate } =
     useDriverLicenseTypeList();
+
+  const [selectedRow, setSelectedRow] = React.useState<DriverLicense>(
+    {} as DriverLicense
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const handleEdit = (row: DriverLicense) => {
+  setSelectedRow(row);
+    setIsDrawerOpen(true);
+  };
+
+  const handleStatusChange = async (
+    row: DriverLicense,
+    isDeleted: boolean
+  ) => {};
+
+  const handleDelete = async (row: DriverLicense) => {
+    try {
+      await apiService.delete(`/driver-license/${row.id}`);
+      mutate();
+      toast.push(
+        <Notification type="info">
+          Licencia de conducir eliminada correctamente
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } catch (error) {
+      console.error('Error deleting a driver license :', error);
+      toast.push(
+        <Notification type="danger">
+          Error al eliminar la licencia de conducir
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } finally {
+      setSelectedRow({} as DriverLicense);
+      setIsDrawerOpen(false);
+    }
+  };
+
+    const handleApply = async (updatedRow: DriverLicense) => {
+      try {
+        await apiService.put(`/driver-license/${selectedRow.id}`, updatedRow);
+        mutate();
+        toast.push(
+          <Notification type="info">
+            Driver license actualizado correctamente
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } catch (error) {
+        console.error('Error updating State:', error);
+        toast.push(
+          <Notification type="danger">
+            Error al actualizar la licencia de conducir
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } finally {
+        setIsDrawerOpen(false);
+        setSelectedRow({} as DriverLicense);
+      }
+    };
 
   const handlePaginationChange = (page: number) => {
     const newTableData = cloneDeep(tableData);
@@ -38,7 +113,7 @@ const DriverLicenseTypeList = () => {
     () => [
       {
         accessorKey: 'name',
-        header: 'Licencias de Conducir',
+        header: 'Departamento',
         cell: (info) => info.getValue(),
       },
       {
@@ -72,28 +147,56 @@ const DriverLicenseTypeList = () => {
             day: '2-digit',
           }),
       },
+      {
+        header: '',
+        id: 'action',
+        cell: (props) => (
+          <ActionTableColumn
+            row={props.row.original}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ),
+      },
     ],
     []
   );
 
   return (
-    <Card>
-      <DataTable
-        columns={columns}
-        data={list}
-        noData={!isLoading && list.length === 0}
-        loading={isLoading}
-        pagingData={{
-          total: total,
-          pageIndex: tableData.pageIndex as number,
-          pageSize: tableData.pageSize as number,
-        }}
-        onPaginationChange={handlePaginationChange}
-        onSelectChange={handleSelectChange}
-        onSort={handleSort}
-      />
-    </Card>
+    <>
+      <Card>
+        <DataTable
+          columns={columns}
+          data={list}
+          noData={!isLoading && list.length === 0}
+          loading={isLoading}
+          pagingData={{
+            total: total,
+            pageIndex: tableData.pageIndex as number,
+            pageSize: tableData.pageSize as number,
+          }}
+          onPaginationChange={handlePaginationChange}
+          onSelectChange={handleSelectChange}
+          onSort={handleSort}
+        />
+      </Card>
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        title="Detalles de la licencia de conducir"
+        closable={true}
+      >
+        <GenericForm
+          initialValues={selectedRow as DriverLicense}
+          onSubmit={(item) => handleApply(item)}
+          onCancel={() => setIsDrawerOpen(false)}
+          submitButtonText="Guardar Cambios"
+          cancelButtonText="Cancelar"
+          subTitle="Complete los detalles de la licencia de conducir"
+        ></GenericForm>
+      </Drawer>
+    </>
   );
 };
 
-export default DriverLicenseTypeList;
+export default DriverLicenseStatusList;
