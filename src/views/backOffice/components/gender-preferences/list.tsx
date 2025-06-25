0cd353e6e6773f193286c4af187d381945360dd7
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-06-25 10:52:04 -0600

    Refactor gender preference list component and update API endpoint for consistency

--- src/views/backOffice/components/gender-preferences/list.tsx
+++ src/views/backOffice/components/gender-preferences/list.tsx
@@ -4,14 +4,90 @@ import DataTable, {
   ColumnDef,
   OnSortParam,
 } from '@/components/shared/DataTable';
-import { useGenderPreferenceList } from '../../hooks';
+import  {useGenderPreferenceList } from '../../hooks';
 import { renderIsDeletedToggle } from '../../../../helpers/renderActiveToggle';
-import { Card } from '@/components/ui';
+import { Card, Drawer, toast } from '@/components/ui';
 import { GenderPreference } from '@prisma/client';
+import GenericForm from '../generic-form';
+import ActionTableColumn from '../generic-form/action-table-column';
+import apiService from '../../../../services/apiService';
+import Notification from '@/components/ui/Notification';
+import { D } from 'framer-motion/dist/types.d-O7VGXDJe';
 
-const GenderPreferenceList = () => {
+const GenderPreferenceStatusList = () => {
   const { list, total, tableData, isLoading, setTableData, mutate } =
     useGenderPreferenceList();
+    console.log('list', list);
+
+  const [selectedRow, setSelectedRow] = React.useState<GenderPreference>(
+    {} as GenderPreference
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const handleEdit = (row: GenderPreference) => {
+  setSelectedRow(row);
+    setIsDrawerOpen(true);
+  };
+
+  const handleStatusChange = async (
+    row: GenderPreference,
+    isDeleted: boolean
+  ) => {};
+
+  const handleDelete = async (row: GenderPreference) => {
+    try {
+      await apiService.delete(`/gender-preferences/${row.id}`);
+      mutate();
+      toast.push(
+        <Notification type="info">
+          Género eliminado correctamente
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } catch (error) {
+      console.error('Error deleting gender preference status:', error);
+      toast.push(
+        <Notification type="danger">
+          Error al eliminar la preferencia de género
+        </Notification>,
+        {
+          placement: 'top-center',
+        }
+      );
+    } finally {
+      setSelectedRow({} as GenderPreference);
+      setIsDrawerOpen(false);
+    }
+  };
+
+    const handleApply = async (updatedRow: GenderPreference) => {
+      try {
+        await apiService.put(`/gender-preferences/${selectedRow.id}`, updatedRow);
+        mutate();
+        toast.push(
+          <Notification type="info">
+            Preferencia de género actualizado correctamente
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } catch (error) {
+        console.error('Error updating gender preference status:', error);
+        toast.push(
+          <Notification type="danger">
+            Error al actualizar la preferencia de  género
+          </Notification>,
+          {
+            placement: 'top-center',
+          }
+        );
+      } finally {
+        setIsDrawerOpen(false);
+        setSelectedRow({} as GenderPreference);
+      }
+    };
 
   const handlePaginationChange = (page: number) => {
     const newTableData = cloneDeep(tableData);
@@ -38,7 +114,7 @@ const GenderPreferenceList = () => {
     () => [
       {
         accessorKey: 'name',
-        header: 'Genero',
+        header: 'Género',
         cell: (info) => info.getValue(),
       },
       {
@@ -72,28 +148,56 @@ const GenderPreferenceList = () => {
             day: '2-digit',
           }),
       },
+      {
+        header: '',
+        id: 'action',
+        cell: (props) => (
+          <ActionTableColumn
+            row={props.row.original}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ),
+      },
     ],
     []
   );
 
   return (
-    <Card>
-      <DataTable
-        columns={columns}
-        data={list}
-        noData={!isLoading && list.length === 0}
-        loading={isLoading}
-        pagingData={{
-          total: total,
-          pageIndex: tableData.pageIndex as number,
-          pageSize: tableData.pageSize as number,
-        }}
-        onPaginationChange={handlePaginationChange}
-        onSelectChange={handleSelectChange}
-        onSort={handleSort}
-      />
-    </Card>
+    <>
+      <Card>
+        <DataTable
+          columns={columns}
+          data={list}
+          noData={!isLoading && list.length === 0}
+          loading={isLoading}
+          pagingData={{
+            total: total,
+            pageIndex: tableData.pageIndex as number,
+            pageSize: tableData.pageSize as number,
+          }}
+          onPaginationChange={handlePaginationChange}
+          onSelectChange={handleSelectChange}
+          onSort={handleSort}
+        />
+      </Card>
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        title="Detalles del Género"
+        closable={true}
+      >
+        <GenericForm
+          initialValues={selectedRow as GenderPreference}
+          onSubmit={(item) => handleApply(item)}
+          onCancel={() => setIsDrawerOpen(false)}
+          submitButtonText="Guardar Cambios"
+          cancelButtonText="Cancelar"
+          subTitle="Complete los detalles de género"
+        ></GenericForm>
+      </Drawer>
+    </>
   );
 };
 
-export default GenderPreferenceList;
+export default GenderPreferenceStatusList;
