4ffea7cd6310595bc255541bf8c3ce08b13e1033
Pablo Godoy <pablogodoy@bairesdev.com>
2025-08-28 20:59:18 -0600

    add user migration

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { userMigration } from './functions/user-migration/resource';
+
+export const backend =
 
 defineBackend({
   auth,
   data,
+  userMigration,
 });
